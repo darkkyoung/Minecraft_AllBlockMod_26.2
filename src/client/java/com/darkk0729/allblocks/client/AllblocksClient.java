5637fdb39311ba197661236b61179c6dfa445f35
김준영 <darkk0729@ajou.ac.kr>
2026-06-30 23:46:57 +0900

    Initial README

---
+++ src/client/java/com/darkk0729/allblocks/client/AllblocksClient.java
@@ -0,0 +1,10 @@
+package com.darkk0729.allblocks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllblocksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
